08db33ab96f92634c36e3bfa05ee895db9d7edf4
timitoch <151081573+timitoch@users.noreply.github.com>
2023-12-01 13:17:54 +0200

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,74 +1,89 @@
-$(document).ready(function() {
-    let words = ["cat", "dog", "book", "chair", "table", "car", "tree", "flower", "road", "city"];
-    let translations = ["кіт", "собака", "книга", "стілець", "стіл", "автомобіль", "дерево", "квітка", "дорога", "місто"];
+$(document).ready(function() { // Коли документ завантажено і готово до взаємодії. Це потрібно, щоб усі елементи сторінки були доступні для маніпуляцій jQuery.
+    let words = {
+        "beginner": ["cat", "dog", "book", "apple", "pen", "car", "tree", "flower", "road", "city"],
+        "intermediate": ["chair", "table", "bottle", "grape", "orange", "lemon", "strawberry", "cherry", "mango", "peach"],
+        "advanced": ["university", "construction", "determination", "development", "establishment", "environment", "administration", "combination", "knowledge", "understanding"]
+    };
+    
+    let translations = {
+        "beginner": ["кіт", "собака", "книга", "яблуко", "ручка", "автомобіль", "дерево", "квітка", "дорога", "місто"],
+        "intermediate": ["стілець", "стіл", "пляшка", "виноград", "апельсин", "лимон", "полуниця", "вишня", "манго", "персик"],
+        "advanced": ["університет", "будівництво", "визначення", "розвиток", "становище", "довкілля", "адміністрація", "комбінація", "знання", "розуміння"]
+    };
+    
+    
     
     let correct = 0;
     let incorrect = 0;
     let currentStep = 0;
-
+    
     function shuffleArray(array1, array2) {
         let index = array1.length;
         let temp, randIndex;
-
         while (0 !== index) {
             randIndex = Math.floor(Math.random() * index);
             index -= 1;
-
-            temp = words[index];
+    
+            temp = words[level][index];
             array1[index] = array1[randIndex];
             array1[randIndex] = temp;
-
+    
             temp = array2[index];
             array2[index] = array2[randIndex];
             array2[randIndex] = temp;
         }
     }
+    
+    let level = $("#levelSelect").val();
 
-    shuffleArray(words, translations);
-
-    $(".front").text(words[currentStep]);
-    $(".back").text(translations[currentStep]);
-
-    $(".card").click(function() {
-        $(this).toggleClass("flipped");
+    $("#levelSelect").change(function() {
+        level = $(this).val();
+        // Оновіть слова і переклади відповідно до нового рівня
+        shuffleArray(words[level], translations[level]);
+        $(".front").text(words[level][currentStep]);
+        $(".back").text(translations[level][currentStep]);
     });
 
-    $("#checkButton").click(function() {
-        let userTranslation = $("#translationInput").val();
-        if (currentStep < 10) {
-            if (userTranslation == translations[currentStep]) {
-                correct++;
-                $("#correctCounter").text("Вірно: " + correct);
-            } else {
-                incorrect++;
-                $("#incorrectCounter").text("Не вірно: " + incorrect);
+
+    $(".card").click(function() { 
+        $(this).toggleClass("flipped"); 
+    });
+
+    $("#checkButton").click(function() { 
+        let userTranslation = $("#translationInput").val(); 
+        if (currentStep < 10) { 
+            if (userTranslation == translations[level][currentStep]) { 
+                correct++; 
+                $("#correctCounter").text("Вірно: " + correct); 
+            } else { 
+                incorrect++; 
+                $("#incorrectCounter").text("Не вірно: " + incorrect); 
             }
-            currentStep++;
-            setTimeout(function() {
-                $(".front").text(words[currentStep]);
-                $(".back").text(translations[currentStep]);
+            currentStep++; 
+            setTimeout(function() { 
+                $(".front").text(words[level][currentStep]);
+        $(".back").text(translations[level][currentStep]);
             }, 300);
-            $(".card").removeClass("flipped");
+            $(".card").removeClass("flipped"); 
         }
-        $("#stepCounter").text("Крок: " + currentStep + "/10");
-        if (currentStep == 10) {
-            let level = correct > 7 ? "Advanced" : correct > 4 ? "Intermediate" : "Beginner";
-            document.getElementById("myModal").innerHTML = "Ваш рівень англійської " + level;
+        $("#stepCounter").text("Крок: " + currentStep + "/10"); 
+        if (currentStep == 10) { 
+            let level = correct > 7 ? "Advanced" : correct > 4 ? "Intermediate" : "Beginner"; 
+            document.getElementById("myModal").innerHTML = "Ваш рівень англійської " + level; 
         }
-        $("#translationInput").val('');
+        $("#translationInput").val(''); // Очищуємо поле введення. Це готує поле введення до наступного кроку.
     });
 
     
-    $("#restartButton").click(function() {
-        correct = 0;
-        incorrect = 0;
-        currentStep = 0;
-        $("#correctCounter").text("Вірно: " + correct);
-        $("#incorrectCounter").text("Не вірно: " + incorrect);
-        $("#stepCounter").text("Крок: " + currentStep + "/10");
-        $(".front").text(words[currentStep]);
-        $(".back").text(translations[currentStep]);
-        document.getElementById("myModal").innerHTML = "Ваш рівень англійської ...";
+    $("#restartButton").click(function() { 
+        correct = 0; 
+        incorrect = 0; 
+        currentStep = 0; 
+        $("#correctCounter").text("Вірно: " + correct); 
+        $("#incorrectCounter").text("Не вірно: " + incorrect); 
+        $("#stepCounter").text("Крок: " + currentStep + "/10"); 
+        $(".front").text(words[level][currentStep]);
+        $(".back").text(translations[level][currentStep]); 
     });
     
 });
